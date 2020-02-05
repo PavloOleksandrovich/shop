3a57e986c6bcf81c6be9f404f8689042ab453e79
Pavlo Makukha <7734.why.not@gmail.com>
2020-02-06 01:55:14 +0200

    [API] Cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
 
 import { connectMongo } from './utils';
 import { config } from '../../common/utils';
 import schema from './schemas';
 
 const server = express();
+
+server.use(cors());
 
 server.use(config.SERVER.GRAPHQL_ENDPOINT, graphqlHTTP({
     schema,
